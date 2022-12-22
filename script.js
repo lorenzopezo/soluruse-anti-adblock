61251d7454008d478e2f4b6638c04bb5ecdeb251
Lorenzo Pezo <lorenzopezomonje@gmail.com>
2022-12-22 15:53:26 -0300

    Done

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-function detectAdBlock(){
-  var adsense = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-  var adblock = false;
-  try {
-    $.getScript(adsense, function(){
-      adblock = false;
-    });
-  }
-  catch(err) {
-    adblock = true;
-  }
-  return adblock;
+function detectAdBlock() {
+    var adblock = false;
+    try {
+        $.getScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function () {
+            adblock = false;
+        });
+    }
+    catch (err) {
+        adblock = true;
+    }
+    return adblock;
 }
 
 if (detectAdBlock()) {
     alert("Por favor desactiva tu adblock para continuar! ^-^");
     window.location.reload();
 }
-
