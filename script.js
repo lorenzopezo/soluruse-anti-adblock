5d63159af0560a1bf90d30454a8020c64293d1ad
lorenzopezo <119983057+lorenzopezo@users.noreply.github.com>
2022-12-22 15:27:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+async function detectAdBlock() {
+  let adBlockEnabled = false
+  const googleAdUrl = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+  try {
+    await fetch(new Request(googleAdUrl)).catch(_ => adBlockEnabled = true)
+  } catch (e) {
+    adBlockEnabled = true
+  } finally {
+    if (adBlockEnabled) {
+        return true
+    }
+        else {
+        return false
+    }
+}
+}
+if (detectAdBlock()) {
+    alert("Por favor desactiva tu adblock para continuar! ^-^");
+    window.location.href = "https://ads."
+}
+
